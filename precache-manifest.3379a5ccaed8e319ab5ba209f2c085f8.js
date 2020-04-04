75a39a8afd98ce28b3183a1e7eba5e4007485c23
Moustafa Ahmed <moustafa.mansour1996@gmail.com>
2020-04-04 18:08:31 +0200

    Updates

---
+++ precache-manifest.3379a5ccaed8e319ab5ba209f2c085f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32456fbda874317ff0cba6dcee1a353c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "629e516b21512a9d9550",
+    "url": "/monsters-rolodex/static/css/main.ee533495.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629e516b21512a9d9550",
+    "url": "/monsters-rolodex/static/js/main.ea2d93b2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
